c081ab49ae9a97ba592e5c674e1f0ec2f272950f
messeili <mymoa.2014@gmail.com>
2020-09-02 13:28:12 +0300

    fix lonitude bug

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const express = require('express');
 // const { request, response } = require('express');
 require('dotenv').config();
 
+const cors = require('cors');
+
+const superagent = require('superagent');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 
 app.get('/', (request, response) => {
     response.status(200).send('you are doing great')
@@ -14,11 +19,18 @@ app.get('/', (request, response) => {
 
 //localhost:3000/location?city=lynwood
 app.get('/location', (req, res) => {
-    const location = require('./data/location.json');
-    console.log(location);
+    // const location = require('./data/location.json');
+    // console.log(location);
     const cityData = req.query.city;
-    let locationData = new Location(cityData, location);
-    res.send(locationData);
+    let key = process.env.LOCATION_KEY;
+    const url = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityData}&format=json`;
+
+    superagent.get(url)
+        .then(data => {
+            let locationData = new Location(cityData, data.body);
+            res.send(locationData);
+        })
+        .catch(error => errorHandler(error, req, res))
 });
 
 app.get('/weather', (req, res) => {
@@ -32,11 +44,13 @@ app.get('/weather', (req, res) => {
     res.status(200).send(allWeather);
 });
 
+
+
 function Location(cityData, location) {
     this.search_query = cityData;
     this.formatted_query = location[0].display_name;
     this.latitude = location[0].lat;
-    this.longtitude = location[0].lon;
+    this.longitude = location[0].lon;
 
 }
 
@@ -52,9 +66,11 @@ app.use('*', (req, res) => {
 
 });
 
-app.use((error, req, res) => {
+function errorHandler(error, req, res) {
     res.status(500).send(error);
-});
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Litening on PORT ${PORT}`)
